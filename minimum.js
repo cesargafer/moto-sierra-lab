7c41d109ca14919c31866aff08bce44a90093fd7
cesargafer <cesargafer@gmail.com>
2016-10-08 09:19:21 -0500

    add min function

---
+++ minimum.js
@@ -0,0 +1,9 @@
+
+function min(num1, num2){
+ if(num1 === num2)
+ console.log('The numbers are equal' + num1 + ' ' + num2 )
+ else if(num1 > num2)
+  console.log('First argument is mayor' + num1)
+ else
+  console.log('Second argument is mayor ' + num2) 
+}
