3da7cf2cde488d5b5bc17160ebab8646ec3d728b
cesargafer <cesargafer@gmail.com>
2016-10-08 09:00:00 -0500

    add bugFix size

--- chessBoard.js
+++ chessBoard.js
@@ -1,8 +1,8 @@
-var size = 8
+var size = 100
 for(var j = 0; j < size - 1; j++){
  if(j % 2 === 0 )
-  console.log('# '.repeat(4))
+  console.log('# '.repeat(size/2))
  else
-  console.log(' #'.repeat(4))
+  console.log(' #'.repeat(size/2))
 }
 
