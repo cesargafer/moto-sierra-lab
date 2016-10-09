d095f1482ead17098d45750b4aa973b90d08baa0
cesargafer <cesargafer@gmail.com>
2016-10-09 14:06:20 -0500

    Add countChar.js

---
+++ countChar.js
@@ -0,0 +1,8 @@
+function countChar(string, char){
+ var count = 0
+ for(var i = 0; i < string.length; i++)
+  if(string.charAt(i) === char)
+ count++
+ return count
+}
+console.log(countChar("kakkerlak", "k"))
