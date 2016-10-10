876900f33d9be4e135e31dcdcc2f8c0ee2cc5828
cesargafer <cesargafer@gmail.com>
2016-10-10 11:06:44 -0500

    Add negative handle

--- rangeW3rdParameter.js
+++ rangeW3rdParameter.js
@@ -3,6 +3,9 @@ function range(start, end, step){
  if(step > 1) 
   for(var i = 0; i <= end; i+=step){ 
    serie.push(i)
+  }else if(step < 1)
+  for(var i = start; i >= end; i+=step){
+   serie.push(i) 
   }else{ 
    for(var i = 0; i <= end; i++)  
    serie.push(i)
@@ -10,5 +13,5 @@ function range(start, end, step){
  return serie
 }
 
-console.log(range(1, 100, 10));
-console.log(range(1, 10));
+console.log(range(0, -100, -10));
+console.log(range(10, -1, -1));
