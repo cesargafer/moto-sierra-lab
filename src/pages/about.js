e35a0afffe9067f5ece0c3dad63b4b9553bfff4c
cesargafer <cesargafer@users.noreply.github.com>
2018-04-29 07:42:04 -0500

    Add Gatsby layout

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+
+export default () => (
+<div>
+ <h1>
+  About
+ </h1>
+ <p>from p</p>
+</div>
+);
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default () => ( 
+<div>
+ <h1>hello from h1</h1>
+ <p> from p </p>
+</div>
+);
+
+
